5b8f51d1620b88daea930cf2a25251b3d15ba1d8
wls.wang <121840415@qq.com>
2022-05-17 16:12:16 +0800

    init

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: WangLiShuai
  * @Date: 2022-05-17 11:15:27
- * @LastEditTime: 2022-05-17 16:09:43
+ * @LastEditTime: 2022-05-17 16:12:00
  * @FilePath: \hot-search\toutiao-trending-hout-search\utils.js
  * @Description: 
  */
@@ -23,11 +23,11 @@ export function createReadMe(words) {
   const readme = fs.readFileSync('./README.md', 'utf-8')
   return readme.replace(/<!-- BEGIN -->[\W\w]*<!-- END -->/, createList(words))
 }
+// ${words.map((x, i) => `${i + 1}. [${x.Title}](${x.Url})`).join("\n")}
 
 export function createList(words) {
   return `<!-- BEGIN -->
   <!-- 最后更新时间 ${new Date()} -->
-  // ${words.map((x, i) => `${i + 1}. [${x.Title}](${x.Url})`).join("\n")}
   ${words.map((x, i) => `${i + 1}. [${x.title}](https://sspai.com/post/${x.id})`)
       .join("\n")}
   <!-- END -->`
