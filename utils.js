836fecd4e431e43b51162ca04f1f6b0335bfc0c1
wls.wang <121840415@qq.com>
2022-05-18 11:35:59 +0800

    init

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 /*
  * @Author: WangLiShuai
  * @Date: 2022-05-17 11:15:27
- * @LastEditTime: 2022-05-18 11:33:33
+ * @LastEditTime: 2022-05-18 11:35:49
  * @FilePath: \hot-search\toutiao-trending-hout-search\utils.js
  * @Description: 
  */
@@ -29,7 +29,7 @@ function createReadMe(words) {
 function createList(words) {
   return `<!-- BEGIN -->
   <!-- 最后更新时间 ${new Date()} -->
-  ${words.map((x, i) => `${i + 1}. [${x.Title}](${x.Url}))`)
+  ${words.map((x, i) => `${i + 1}. [${x.Title}](${x.Url})`)
       .join("\n")}
   <!-- END -->`
 }
